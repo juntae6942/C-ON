6817168bbee1964bee8d7b503651368a055e894d
potato <65760583+juntae6942@users.noreply.github.com>
2024-05-30 17:01:50 +0900

    init

---
+++ src/main/java/CON/CON/COnApplication.java
@@ -0,0 +1,13 @@
+package CON.CON;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class COnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(COnApplication.class, args);
+	}
+
+}
